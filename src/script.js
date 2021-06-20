7bfaf060bdb814c329fab75bf85d2f3141c00ced
Bruno Gomes <brunogomes.dev@yahoo.com>
2021-06-20 16:35:16 -0300

    add more functions

--- src/script.js
+++ src/script.js
@@ -1,17 +1,50 @@
-const animalImg = document.querySelectorAll('img')
-const allImg = animalImg.forEach((img) => console.log(img))
+const menu = document.querySelector('.menu')
 
-const uniqAnimalImg = document.querySelectorAll('img[src^="img/imagem"]')
-const allUniqImg = uniqAnimalImg.forEach((img) => console.log(img))
+if (!menu.classList.contains('ativo')) {
+    menu.classList.add('nao-possui-ativo')
+} else {
+    menu.classList.add('possui-ativo')
+}
 
-const linkInterno = document.querySelectorAll('[href^="#"]')
-console.log(linkInterno)
+menu.className += ' azul'
+console.log(menu.className)
 
-const firstH2 = document.querySelector('.animais-descricao h2')
-console.log(firstH2)
+// attributes
 
-const allP = document.querySelectorAll('p')
-const lastP = allP[allP.length - 1]
-console.log(lastP)
+const animais = document.querySelector('.animais')
+console.log(animais.attributes.class)
 
-allP.forEach((p) => console.log(p.innerText))
+const img = document.querySelector('img')
+console.log(img.getAttribute('src'))
+
+img.setAttribute('alt', 'É uma raposa')
+console.log(img.getAttribute('alt'))
+
+const possuiAlt = img.hasAttribute('alt')
+console.log(possuiAlt)
+
+allMenu = document.querySelectorAll('.menu')
+allMenu.forEach((el) => {
+    el.classList.add('ativo')
+})
+console.log(allMenu)
+
+allMenu.forEach((el) => {
+    el.classList.remove('ativo')
+})
+allMenu[0].classList.add('ativo')
+console.log(allMenu)
+
+imgs = document.querySelectorAll('img')
+imgs.forEach((img) => {
+    if (img.hasAttribute('alt')) {
+        console.log('essa imagem possui atributo alt')
+    } else {
+        console.log('essa imagem não possui atributo alt')
+    }
+})
+
+const hrefMenu = menu.querySelectorAll('li')
+const hrefDev = hrefMenu[hrefMenu.length - 1]
+const hrefLink = hrefDev.querySelector('a')
+hrefLink.setAttribute('href', 'https://github.com/BrunoSGomes')
