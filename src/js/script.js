63929851a93438219d035139b2f76a6c3652eb29
Bruno Gomes <brunogomes.dev@yahoo.com>
2022-05-04 21:21:32 -0300

    chore(project): finish login modal and create map tooltip

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,12 @@ import initSmoothScroll from "./modules/scroll-suave.js"
 import initScrollAnimation from "./modules/scroll-animacao.js"
 import initAccordionList from "./modules/accordion.js"
 import initTabNav from "./modules/tabnav.js"
+import initModal from "./modules/modal.js"
+import initTooltip from "./modules/tooltip.js"
 
 initSmoothScroll()
 initScrollAnimation()
 initAccordionList()
 initTabNav()
+initModal()
+initTooltip()
