d487803a8f2629c58f8ca14699ed39e6c3c2b666
Bruno Gomes <brunogomes.dev@yahoo.com>
2022-08-03 00:25:29 -0300

    refactor(Modal): transform modal into class

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 import SmoothScroll from './modules/scroll-suave.js';
 import AccordionList from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
+import Modal from './modules/modal.js';
 
 import initScrollAnimation from './modules/scroll-animacao.js';
-import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
@@ -20,8 +20,10 @@ accordionList.init();
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
 
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
+
 initScrollAnimation();
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
