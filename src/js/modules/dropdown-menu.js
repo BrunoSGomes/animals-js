1387994b58b1c96a648bcb7e484e31e0779ffef1
Bruno Gomes <brunogomes.dev@yahoo.com>
2022-08-10 00:38:38 -0300

    refactor(DropdownMenu): transform into class

--- src/js/modules/dropdown-menu.js
+++ src/js/modules/dropdown-menu.js
@@ -1,13 +1,35 @@
 import outsideClick from './outside-click.js';
 
-export default function initDropdownMenu() {
-    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
+    constructor(dropdownMenus, events) {
+        this.dropdownMenus = document.querySelectorAll(dropdownMenus);
 
-    function handleClick(event) {
-        event.preventDefault();
-        this.classList.add('active');
-        outsideClick(this, ['touchstart', 'click'], () => this.classList.remove('active'));
+        if (events === undefined) this.events = ['touchstart', 'click'];
+        else this.events = events;
+
+        this.activeClass = 'active';
+        this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
     }
 
-    dropdownMenus.forEach((menu) => ['touchstart', 'click'].forEach((userEvent) => menu.addEventListener(userEvent, handleClick)));
+    init() {
+        if (this.dropdownMenus.length) {
+            this.addDropdownMenusEvent();
+        }
+        return this;
+    }
+
+    addDropdownMenusEvent() {
+        this.dropdownMenus.forEach((menu) => {
+            this.events.forEach((userEvent) => {
+                menu.addEventListener(userEvent, this.activeDropdownMenu);
+            });
+        });
+    }
+
+    activeDropdownMenu(event) {
+        event.preventDefault();
+        const element = event.currentTarget;
+        element.classList.add(this.activeClass);
+        outsideClick(element, this.events, () => element.classList.remove(this.activeClass));
+    }
 }
