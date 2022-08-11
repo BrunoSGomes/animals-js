7de15046bfc9901708ecf9fd1c505c216f940d1f
Bruno Gomes <brunogomes.dev@yahoo.com>
2022-08-10 22:44:29 -0300

    chore(deploy): update file location to deploy github pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/js/script.js',
     output: {
-        path: path.resolve(__dirname, './src'),
+        path: path.resolve(__dirname, './'),
         filename: 'main.js',
     },
     module: {
